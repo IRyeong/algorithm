30aa1692e2fc1246331381f3fd92400d8f62e9c2
IRyeong <31383362+IRyeong@users.noreply.github.com>
2022-10-15 17:21:31 +0900

    [Platinum IV] Title: 도망자 원숭이, Time: 1396 ms, Memory: 50912 KB -BaekjoonHub

--- 백준/Platinum/1602. 도망자 원숭이/도망자 원숭이.java
+++ 백준/Platinum/1602. 도망자 원숭이/도망자 원숭이.java
@@ -8,7 +8,7 @@ public class Main {
 	static int INF = 987654321;
 	static int N, M, Q;
 	static Pair teasing[]; // 멍멍이가 원숭이를 괴롭히는 시간
-	static long fw[][][]; // 0 : 플로이드 워셜을 이용해 구한 꼭짓점부터 꼭짓점까지 가는데 걸리는 시간, 1 : 놀리는 최대 시간
+	static int fw[][][]; // 0 : 플로이드 워셜을 이용해 구한 꼭짓점부터 꼭짓점까지 가는데 걸리는 시간, 1 : 놀리는 최대 시간
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +18,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
-		fw = new long[N + 1][N + 1][2];
+		fw = new int[N + 1][N + 1][2];
 		// 각 거리 초기값 최대로 해주기
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
@@ -31,8 +31,8 @@ public class Main {
 			}
 		}
 		teasing = new Pair[N + 1];
+		teasing[0] = new Pair(0, 0);
 		st = new StringTokenizer(br.readLine());
-		teasing[0] = new Pair(0,0);
 		for (int i = 1; i <= N; i++) {
 			teasing[i] = new Pair(i, Integer.parseInt(st.nextToken()));
 		}
@@ -47,18 +47,16 @@ public class Main {
 			fw[a][b][1] = Math.max(teasing[a].time, teasing[b].time);
 			fw[b][a][1] = Math.max(teasing[a].time, teasing[b].time);
 		}
+		// 놀리는 시간(가중치) 기준으로 오름차순 정렬
 		Arrays.sort(teasing);
-//		for (int i = 0; i <= N; i++) {
-//			System.out.println(teasing[i].num+", "+teasing[i].time);
-//		}
 		// 플로이드 워셜로 거리 모두 구해두기
 		for (Pair p : teasing) {
-			if(p.num==0) {
+			if (p.num == 0) { // 0인 마을은 없으므로 패스
 				continue;
 			}
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
-					long tmpMax = max(fw[i][p.num][1], fw[p.num][j][1], p.time);
+					int tmpMax = max(fw[i][p.num][1], fw[p.num][j][1], p.time);
 					if (fw[i][j][0] + fw[i][j][1] > fw[i][p.num][0] + fw[p.num][j][0] + tmpMax) {
 						fw[i][j][0] = fw[i][p.num][0] + fw[p.num][j][0];
 						fw[i][j][1] = tmpMax;
@@ -82,22 +80,25 @@ public class Main {
 		System.out.println(sb);
 	}
 
-	private static class Pair implements Comparable<Pair>{
-		int num; // 마을 번호 
-		int time; // 놀리는 시간 
+	private static class Pair implements Comparable<Pair> {
+		int num; // 마을 번호
+		int time; // 놀리는 시간
+
 		public Pair(int num, int time) {
 			super();
 			this.num = num;
 			this.time = time;
 		}
+
 		@Override
 		public int compareTo(Pair o) {
-			return this.time - o.time; // 놀리는 시간 기준 오름차순 
+			return this.time - o.time; // 놀리는 시간 기준 오름차순
 		}
 	}
-	
-	private static long max(long a, long b, int c) {
-		long ret;
+
+	// 세 수 중 가장 큰 수 리턴
+	private static int max(int a, int b, int c) {
+		int ret;
 		ret = Math.max(a, b);
 		ret = Math.max(ret, c);
 		return ret;
