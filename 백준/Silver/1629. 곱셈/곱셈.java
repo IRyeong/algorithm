299124b66a48f519297e1f1c0202c3152c5cceac
IRyeong <31383362+IRyeong@users.noreply.github.com>
2023-03-05 22:51:52 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -17,25 +17,15 @@ public class Main {
 			tmp /= 2;
 			cnt++;
 		}
-//		System.out.println("cnt : " + cnt);
-		// A를 2의 i승 만큼 곱한 숫자를 C로 나눈 나머지
 		long arr[] = new long[cnt + 1];
-		// A를 C로 나눈 나머지로 갱신
 		A %= C;
-		// A를 2의 0승인 1개만큼 곱한 숫자를 C로 나눈 나머지는 그대로 A
 		arr[0] = A;
-		//
 		for (int j = 1; j <= cnt; j++) {
 			arr[j] = (arr[j - 1] * arr[j - 1]) % C;
 		}
-//		for (int i = 0; i <= cnt; i++) {
-//			System.out.print(arr[i]+" ");
-//		}
-//		System.out.println();
 		long ret = 1L;
 		for (int i = 0; i <= cnt; i++) {
 			if ((B & (1 << i)) != 0) {
-//				System.out.println("i : " + i);
 				ret = (ret * arr[i]) % C;
 			}
 		}
